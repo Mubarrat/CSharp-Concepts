c748913ccd4c7e176b2dff986aca01a8eb70d2ba
Mubarrat <Mubarrati@outlook.com>
2024-06-14 17:30:02 +0600

    Update transformed.cs

--- SingletonClasses/transformed.cs
+++ SingletonClasses/transformed.cs
@@ -6,7 +6,7 @@ public class SingletonExampleClass
     private SingletonExampleClass()
     {
         if (_lazyInstance.IsValueCreated) // Checked if it is called for a second time if it has one instance.
-		    throw new InvalidOperationException("Singleton already initialized.");
+            throw new InvalidOperationException("Singleton classes can only have one instance.");
         
         // Do work here.
     }
