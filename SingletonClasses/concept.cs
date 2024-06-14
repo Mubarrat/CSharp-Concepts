c61425ddcb4dce81d2b05d9e61f7ca56d31f8d14
Mubarrat <Mubarrati@outlook.com>
2024-06-14 17:11:39 +0600

    Create concept.cs

---
+++ SingletonClasses/concept.cs
@@ -0,0 +1,10 @@
+public singleton class SingletonExampleClass
+{
+    // You can't add parameters or overload constructor.
+    // You can define singleton classes without a constructor.
+    // Singleton classes' constructor must be private.
+    private SingletonExampleClass()
+    {
+        // Do work here.
+    }
+}
