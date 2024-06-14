009501caa58c8241cc760630af31efb7c1dea539
Mubarrat <Mubarrati@outlook.com>
2024-06-14 16:24:47 +0600

    Update concept.cs

--- ThrowArgumentNullExceptionAttribute/concept.cs
+++ ThrowArgumentNullExceptionAttribute/concept.cs
@@ -15,7 +15,7 @@ public class NullCheckAttributeExample
 	{
 		// Similarly for any method when ThrowArgumentNullExceptionAttribute is used on the parameter, it'll throw an exception if the parameter is null.
 		return new NullCheckAttributeExample(id, argument);
-    }
+	}
 
 	// Note: ThrowArgumentNullExceptionAttribute can only be used on a parameter.
 	// It can't be used on a parameter whose reference types are non-nullable because they can't be passed with null.
