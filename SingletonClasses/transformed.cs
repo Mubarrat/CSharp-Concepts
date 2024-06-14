c68c31ba3fa2e0a9028b57dc68e1aa334acc1dc6
Mubarrat <Mubarrati@outlook.com>
2024-06-14 17:23:10 +0600

    Create transformed.cs

---
+++ SingletonClasses/transformed.cs
@@ -0,0 +1,15 @@
+public class SingletonExampleClass
+{
+	private static readonly Lazy<SingletonExampleClass> _lazyInstance =
+        new Lazy<SingletonExampleClass>(() => new SingletonExampleClass(), true);
+    
+    private SingletonExampleClass()
+    {
+        if (_lazyInstance.IsValueCreated) // Checked if it is called for a second time if it has one instance.
+		    throw new InvalidOperationException("Singleton already initialized.");
+        
+        // Do work here.
+    }
+    
+	public static MySingleton Instance => _lazyInstance.Value;
+}
