3ab2afc53586b7cee51124a0edfcec5ef2b44a3d
Mubarrat <Mubarrati@outlook.com>
2024-06-15 12:28:19 +0600

    Update concept.cs

--- PrimaryUnnamedConstructorInitializer/concept.cs
+++ PrimaryUnnamedConstructorInitializer/concept.cs
@@ -1,11 +1,12 @@
-// "public()" isn't neccesary but you can
-// use that to change access level of constructor
+// "public()" isn't necessary but you can
+// Use that to change the access level of the constructor
 public class FileProcessor(string filePath) : public()
 {
-    // Unnamed Initializer. But unnamed initializer must
-    // come first before any method, field or property.
-    // Or compiler wouldn't detect it as unnamed initializer.
-    // Code blocking isn't neccesary here.
+    // Unnamed Initializer. But the unnamed initializer must
+    // come first before any method, field, or property.
+    // Or the compiler wouldn't detect it as an unnamed initializer.
+    // Code blocking isn't necessary here.
+    // It can also have multiple lines without code block.
     ValidateFilePath(filePath);
     
     public string FilePath { get; set; } = filePath;
